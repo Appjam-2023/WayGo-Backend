f75517a0b9cb62714ffbbfd52ff27bea128599e7
KimTaeO <taeo0328@gmail.com>
2023-08-05 21:04:24 +0900

    :sparkles: :: Spring Security 인증객체 작성

---
+++ src/main/kotlin/com/helpme/waygo/global/security/auth/AuthDetails.kt
@@ -0,0 +1,23 @@
+package com.helpme.waygo.global.security.auth
+
+import com.helpme.waygo.auth.entity.User
+import org.springframework.security.core.GrantedAuthority
+import org.springframework.security.core.userdetails.UserDetails
+
+class AuthDetails(
+    private val user: User
+) : UserDetails {
+    override fun getAuthorities(): MutableCollection<out GrantedAuthority> = user.userRole
+
+    override fun getPassword(): String = user.password
+
+    override fun getUsername(): String = user.phoneNum
+
+    override fun isAccountNonExpired(): Boolean = true
+
+    override fun isAccountNonLocked(): Boolean = true
+
+    override fun isCredentialsNonExpired(): Boolean = true
+
+    override fun isEnabled(): Boolean = true
+}
